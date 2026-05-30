c86693abf1453798e5edcc620130fbf1c62ababd
Adarsh Sharma <adarsh.sharma@reqpedia.com>
2026-05-30 21:39:09 +0530

    Dependencies install.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-600">
+        Mathematics Practice Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
